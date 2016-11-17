b294ef4061079f1655ed154e0628c876a982fb4b
jtclaypool <jtclaypool@ucdavis.edu>
2016-11-16 21:21:16 -0800

    updated with output for mesh with ggplot for heatmap

--- R/eigenvector_correlation.R
+++ R/eigenvector_correlation.R
@@ -4,17 +4,24 @@
 eigen.correlation<-function(data="relative abundance",community="community detection",
                             metadata="metadata file",categories="environments to compare"){
   require(WGCNA)
+  require(reshape)
   data.netw=data[,which(names(data)%in%community$names)]
-  ME=moduleEigengenes(data,colors=community$membership)
+  ME=moduleEigengenes(data.netw,colors=community$membership)
   corr=data.frame(matrix(0,ncol=length(unique(community$membership)),nrow = length(categories)))
-  names(corr)<-order(unique(community$membership))
+  names(corr)<-sort(unique(community$membership),decreasing = F)
   rownames(corr)<-categories
-  pval=data.frame(matrix(0,ncol=length(unique(community$membership)),nrow = length(categories)))
+  pval=corr
+  print(corr)
+  print(categories)
   for(i in 1:length(categories)){
     for(j in 1:length(unique(community$membership))){
-      test=cor.test(vfas,datME$eigengenes[,i])
-      corr[j]=test$estimate
-      pval[j]=test$p.value
+      test=cor.test(metadata[,which(names(metadata)%in%categories[i])],ME$eigengenes[,j])
+      corr[i,j]=test$estimate
+      pval[i,j]=test$p.value
     }
   }
+  newcorr=melt(corr)
+  newpval=melt(pval)
+  dat=cbind.data.frame(newcorr,"pval"=newpval$value,"category"=rep(rownames(corr),nrow(newcorr)/length(categories)))
+  return(list("corr"=corr,"pval"=pval,"melt_cor"=dat))
 }
