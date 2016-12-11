330a16e1e8c9a05cd560b66add16f6fd45be860a
jtclaypool <jtclaypool@ucdavis.edu>
2016-12-11 14:24:42 -0800

    improved otu ID information and taxonomy from raw file

--- R/read_biom.R
+++ R/read_biom.R
@@ -1,11 +1,18 @@
 #'
 #' #Read in biom file and return relative abundance
 read.biom<-function(biom="biom"){
-  #for eventually
+  #taxonomy and OTU information
   taxon=biom$ConsensusLineage
   taxon=do.call("rbind",strsplit(as.character(taxon),';'))
   taxon=data.frame(apply(taxon,2,as.character))
   names(taxon)=c("Kingdom","Phylum","Class","Order","Family","Genus","Species")
+  otus=biom[,1]
+  taxon=cbind(otus,taxon)
+  rownames(biom) <- otus
+
+  #remove taxonomy column
+  biom=biom[,-(c(1,ncol(biom)))]
+
   #remove singletons and OTU's with no counts
   biom[biom==1]<-0
   biom.trim=biom[-(which(rowSums(biom)==0)),]
@@ -17,5 +24,5 @@ read.biom<-function(biom="biom"){
 
   #remove rows that never account for more than 0.5% of a sample
   per.trial.trim=per.trial[,apply(per.trial,2,max)>.5]
-  return("RA.Otus"=per.trial.trim)
+  return(list("RA.Otus"=per.trial.trim,"taxon"=taxon))
 }
