d7df29531876d1a6f763b69b0f4a638fd3e43cd6
jtclaypool <jtclaypool@ucdavis.edu>
2017-01-18 13:56:24 -0800

    match name vectors

--- R/eigenvector_correlation.R
+++ R/eigenvector_correlation.R
@@ -6,13 +6,14 @@ eigen_correlation<-function(data="relative abundance",community="community detec
   require(WGCNA)
   require(reshape)
   data.netw=data[,which(names(data)%in%community$names)]
+  community_ord=community$membership[match(colnames(data),community$names)]
   ME=moduleEigengenes(data.netw,colors=community$membership)
-  corr=data.frame(matrix(0,ncol=length(unique(community$membership)),nrow = length(categories)))
+  corr=data.frame(matrix(0,ncol=length(unique(community_ord)),nrow = length(categories)))
   names(corr)<-sort(unique(community$membership),decreasing = F)
   rownames(corr)<-categories
   pval=corr
   for(i in 1:length(categories)){
-    for(j in 1:length(unique(community$membership))){
+    for(j in 1:length(unique(community_ord))){
       test=cor.test(metadata[,which(names(metadata)%in%categories[i])],ME$eigengenes[,j])
       corr[i,j]=test$estimate
       pval[i,j]=test$p.value
