a89c0ffbd6bc2bfe98b77304d4b8c2a13e2659ed
jtclaypool <jtclaypool@ucdavis.edu>
2017-01-25 15:06:15 -0800

    Update barplot_module for meta matching

--- R/barplot_module.R
+++ R/barplot_module.R
@@ -4,15 +4,14 @@ barplot_module<-function(data="relative abundance",meta="treatment metadata",cat
   require(reshape)
   require(ggplot2)
   require(RColorBrewer)
-  mean.otus=as.data.frame(aggregate(data,by=list(meta[,which(colnames(meta)%in%categories)]),mean))
-  rownames(mean.otus)=mean.otus[,1]
-  mean.otus=t(mean.otus[,-1])
-
-  colr1=(rownames(mean.otus)%in%niche$names)*1
-  colr1[(1:nrow(mean.otus))[(rownames(mean.otus)%in%niche$names)]]<-niche$membership
+  meta=meta[match(rownames(data),meta$Label),]
+  mean.otus = as.data.frame(aggregate(data, by = list(meta[,which(colnames(meta) %in% categories)]), mean))
+  rownames(mean.otus) = mean.otus[, 1]
+  mean.otus = t(mean.otus[, -1])
+  sum(!is.na(match(rownames(mean.otus),niche$names))
+  colr1=niche$membership[match(rownames(mean.otus),niche$names)]
+  colr1[is.na(colr1)] <- 0
   colrs=brewer.pal(max(colr1+1),"Set3")[colr1+1]
-  #bar=barplot(mean.otus[order(colr1),],col=colrs[order(colr1)],border=NA,las=1)
-  #legend(2,115,legend=unique(colr1+1),fill=unique(colrs),horiz=T)
   ggOTUs=cbind.data.frame(mean.otus,"Cluster"=paste(rep("Cluster",times=length(colr1)),colr1))
   levels(ggOTUs$Cluster)[1]="No Cluster"
   bar_data=melt(ggOTUs,id="Cluster")
