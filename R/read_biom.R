cc5df13306b82165bf4ccba2c8ef1961448eff95
jtclaypool <jtclaypool@ucdavis.edu>
2017-02-24 09:34:24 -0800

    hopefully updated to read in metagenome files from PICRUSt

--- R/read_biom.R
+++ R/read_biom.R
@@ -1,15 +1,18 @@
 #'
 #' #Read in biom file and return relative abundance
-read.biom<-function(biom="biom",new=T){
+read.biom<-function(biom="biom",new=T,metagenome=F){
   if(new){biom <- read.table(biom,header=T,sep="\t",comment.char="",skip=1)}
+  if(new & metgenome){biom <- read.table(biom,header=T,sep="\t",comment.char="",skip=1,quote="")}
   #taxonomy and OTU information
-  taxon=biom$ConsensusLineage
-  taxon=do.call("rbind",strsplit(as.character(taxon),';'))
-  taxon=data.frame(apply(taxon,2,as.character))
-  names(taxon)=c("Kingdom","Phylum","Class","Order","Family","Genus","Species")
-  otus=biom[,1]
-  taxon=cbind(otus,taxon)
-  rownames(biom) <- otus
+  if(!metagenome){
+    taxon=biom$ConsensusLineage
+    taxon=do.call("rbind",strsplit(as.character(taxon),';'))
+    taxon=data.frame(apply(taxon,2,as.character))
+    names(taxon)=c("Kingdom","Phylum","Class","Order","Family","Genus","Species")
+    otus=biom[,1]
+    taxon=cbind(otus,taxon)
+    rownames(biom) <- otus
+  }
 
   #remove taxonomy column
   biom=biom[,-(c(1,ncol(biom)))]
@@ -27,6 +30,7 @@ read.biom<-function(biom="biom",new=T){
   col.sums=apply(biom.trim,2,sum)
   per.trial=sweep(biom.trim,2,col.sums,"/")
   per.trial=as.data.frame(t(per.trial)*100)
-
-  return(list("RA.Otus"=per.trial,"taxon"=taxon,"biom_tab"=biom_tab))
+  
+  if(!metagenome){return(list("RA.Otus"=per.trial,"taxon"=taxon,"biom_tab"=biom_tab))}
+  if(metagenome){return(list("RA.genes"=per.trial))}
 }
