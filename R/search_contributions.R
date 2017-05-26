068b3e6cbbe6089520a5a256efb7010b2199c424
jtclaypool <jtclaypool@ucdavis.edu>
2017-05-26 10:57:54 -0700

    comment out for now

--- R/search_contributions.R
+++ R/search_contributions.R
@@ -1,8 +1,8 @@
-search_contributions <- function(data="PICRUSt_contribution", by="OTU",gene_subset=F,order="Ascending",){
-  contrib <- read.table(f,header=T,sep=",")
-contrib$category <- meta$Treatment[match(contrib$Sample,meta$Label)]
+#search_contributions <- function(data="PICRUSt_contribution", by="OTU",gene_subset=F,order="Ascending",){
+#  contrib <- read.table(f,header=T,sep=",")
+#contrib$category <- meta$Treatment[match(contrib$Sample,meta$Label)]
 
-test=(contrib[match(contrib$Sample,meta$Label[which(meta$Treatment%in%"Compost")])>0,])
-test=droplevels(test[!is.na(test$Gene),])
-test=aggregate(contrib,by=list(contrib$category,contrib$Gene,contrib$OTU),FUN = mean)
-}
+#test=(contrib[match(contrib$Sample,meta$Label[which(meta$Treatment%in%"Compost")])>0,])
+#test=droplevels(test[!is.na(test$Gene),])
+#test=aggregate(contrib,by=list(contrib$category,contrib$Gene,contrib$OTU),FUN = mean)
+#}
